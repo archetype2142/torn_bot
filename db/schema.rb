e71505ad353fea044d066ea4146a82b0a3dfce8f
ritwick malhotra <malhotraritwick2011@gmail.com>
2024-06-09 21:35:51 +0200

    added pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,9 @@
 # It's strongly recommended that you check this file into your version control system.
 
 ActiveRecord::Schema[7.1].define(version: 2024_06_09_190004) do
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "tracked_assets", force: :cascade do |t|
     t.string "type"
     t.string "asset_number"
