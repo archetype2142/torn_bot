e4aa62dd5ffc9c65c4d7c9fc59aed5061594c51d
ritwick malhotra <malhotraritwick2011@gmail.com>
2024-06-11 00:11:25 +0200

    scheduled sidekiq

--- config/initializers/sidekiq_scheduler.rb
+++ config/initializers/sidekiq_scheduler.rb
@@ -7,7 +7,7 @@ puts "defined?(Rails::Server) is #{defined?(Rails::Server).inspect}"
 SidekiqScheduler::Scheduler.instance.enabled = true
 Sidekiq.configure_server do |config|
   config.on(:startup) do
-    Sidekiq.schedule = YAML.load_file(File.expand_path('../../sidekiq_scheduler.yml', __FILE__))
+    Sidekiq.schedule = YAML.load_file(Rails.root.join('config/sidekiq_scheduler.yml'))
     SidekiqScheduler::Scheduler.instance.reload_schedule!
   end
 end
